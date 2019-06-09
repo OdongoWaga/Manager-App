b6351d06c6e20f544a995e84b883497468bc00ed
Waga <wagaodongo@gmail.com>
2019-06-09 12:23:08 +0300

    add leads

--- manager/frontend/src/reducers/leads.js
+++ manager/frontend/src/reducers/leads.js
@@ -1,11 +1,13 @@
 import {
     GET_LEADS,
-    DELETE_LEAD
-} from '../actions/types'
+    DELETE_LEAD,
+    ADD_LEAD,
+    CLEAR_LEADS
+} from "../actions/types.js";
 
 const initialState = {
     leads: []
-}
+};
 
 export default function (state = initialState, action) {
     switch (action.type) {
@@ -18,8 +20,18 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 leads: state.leads.filter(lead => lead.id !== action.payload)
-            }
-            default:
-                return state;
+            };
+        case ADD_LEAD:
+            return {
+                ...state,
+                leads: [...state.leads, action.payload]
+            };
+        case CLEAR_LEADS:
+            return {
+                ...state,
+                leads: []
+            };
+        default:
+            return state;
     }
 }
