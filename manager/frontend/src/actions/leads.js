acdb06c5de0adba59fdb46128c70ec0b37fd6ace
Waga <wagaodongo@gmail.com>
2019-06-09 12:17:43 +0300

    leads

--- manager/frontend/src/actions/leads.js
+++ manager/frontend/src/actions/leads.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 import {
-    GET_LEADS
+    GET_LEADS,
+    DELETE_LEAD,
+    ADD_LEAD
 } from "./types";
 
 //GET LEADS
@@ -16,3 +18,36 @@ export const getLeads = () => dispatch => {
         })
         .catch(err => console.log(err))
 }
+// DELETE LEAD
+export const deleteLead = id => (dispatch, getState) => {
+    axios
+        .delete(`/api/leads/${id}/`, tokenConfig(getState))
+        .then(res => {
+            dispatch(createMessage({
+                deleteLead: "Lead Deleted"
+            }));
+            dispatch({
+                type: DELETE_LEAD,
+                payload: id
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+// ADD LEAD
+export const addLead = lead => (dispatch, getState) => {
+    axios
+        .post("/api/leads/", lead, tokenConfig(getState))
+        .then(res => {
+            dispatch(createMessage({
+                addLead: "Lead Added"
+            }));
+            dispatch({
+                type: ADD_LEAD,
+                payload: res.data
+            });
+        })
+        .catch(err =>
+            dispatch(returnErrors(err.response.data, err.response.status))
+        );
+};
--- manager/frontend/src/reducers/leads.js
+++ manager/frontend/src/reducers/leads.js
@@ -1,15 +1,23 @@
-import {GET_LEADS} from '../actions/types'
+import {
+    GET_LEADS,
+    DELETE_LEAD
+} from '../actions/types'
 
 const initialState = {
-    leads:[]
+    leads: []
 }
 
-export default function(state = initialState, action) {
-    switch(action.type) {
+export default function (state = initialState, action) {
+    switch (action.type) {
         case GET_LEADS:
-            return{
+            return {
                 ...state,
                 leads: action.payload
+            };
+        case DELETE_LEAD:
+            return {
+                ...state,
+                leads: state.leads.filter(lead => lead.id !== action.payload)
             }
             default:
                 return state;
